62b19bae42650d4a6f98168391b0ddccc9380f99
guybarnahum <guy@barnahum.com>
2015-12-24 15:49:26 -0800

    access arrays by array key

--- gitevent.php
+++ gitevent.php
@@ -45,9 +45,9 @@ if (!empty($postdata)) {
         $data  = [];
     }
     
-    $repo = isset( $data.repository )? $data.repository : [] ;
+    $repo = isset( $data[ 'repository' ])? $data[ 'repository' ] : [] ;
     $msg[] = 'POST recieved for ' . print_r(repo, true) ;
-    $repo  = isset($repo.name)? $repo.name : null;
+    $repo  = isset($repo[ 'name' ])? $repo[ 'name' ] : null;
 }
 else
 // or maybe we are just in debug mode?
