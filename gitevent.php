abd3746d23f7dcd47234a71732dfd9555abc5d68
guybarnahum <guy@barnahum.com>
2016-01-03 19:21:44 -0800

    workaround __DIR__ error

--- gitevent.php
+++ gitevent.php
@@ -6,14 +6,15 @@ require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
     
 function get_version( $touch = false )
 {
-    $ver   = file_get_contents( __DIR__ . '/version.txt' );
-    $build = file_get_contents( __DIR__ . '/build.txt'   );
+    $cwd   = __DIR__;
+    $ver   = file_get_contents( "$cwd/version.txt" );
+    $build = file_get_contents( "$cwd/build.txt"   );
     
     $num = intval( $build ); // its 0 in case of missing or invalid value
     
     if ( $touch ){
         $num += 1;
-        file_put_contents( __DIR__ . '/build.txt' , $num  );
+        file_put_contents( './build.txt' , $num  );
     }
     
     return "$ver$num";
