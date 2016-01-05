511d0d1aa24cb2e541473d7118f508de3c4ee298
guybarnahum <guy@barnahum.com>
2016-01-04 16:33:56 -0800

    redirect to index-alt

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?
- Header( "HTTP/1.1 301 Moved Permanently" );
- Header( "Location: http://reactor.barnahum.com/index-alt.html" );
- ?>
+<?php
+
+/*
+ Make sure this file is set as DirectoryIndex in .htaccess
+ DirectoryIndex __FILE__
+*/
+
+
+Header( "HTTP/1.1 301 Moved Permanently" );
+Header( "Location: http://reactor.barnahum.com/index-alt.html" );
+?>
