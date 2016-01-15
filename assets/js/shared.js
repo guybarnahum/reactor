1450bd8b60fcca071b3e444a05e857ef2041a5b0
guybarnahum <guy@barnahum.com>
2016-01-14 18:37:45 -0800

    add Oded's startup advisers

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,37 +1,47 @@
 // Annotator
 
-$('.container').annotator()
-               .annotator('setupPlugins', null, {
-                    Auth: { 
-                        tokenUrl: 'http://reactor.barnahum.com/jwt/token.php'
-                    },
-                    Filter: false,
-                    Permissions: false,
-                    AnnotateItPermissions: {}
-            });
-
 function isString(x) {
     return x !== null &&
            x !== undefined && 
            x.constructor === String
 }
 
-// version ajax
+function isLocal(){
+    return (window.location.protocol == 'file:');
+}
 
-var url  = 'http://reactor.barnahum.com/version.php';
- 
-// call back function from url
-$.getJSON( url, function(data) {
-    
-                    if ( isString( data ) ){
-                        
-                        console.log( 'version data :' + data );
-                        
-                        if (data.length > 12){
-                            data = data.substr(0,11)+'&hellip;';
-                            console.log( 'version data shorten to :' + data );
-                        }
-                        
-                        $('.version').text( data );
-                    }
+function init_common()
+{
+    $('.container').annotator()
+                   .annotator('setupPlugins', null, {
+                        Auth: { 
+                            tokenUrl: 'http://reactor.barnahum.com/jwt/token.php'
+                        },
+                        Filter: false,
+                        Permissions: false,
+                        AnnotateItPermissions: {}
                 });
+
+
+    // version ajax
+
+    var url  = 'http://reactor.barnahum.com/version.php';
+ 
+    // call back function from url
+    $.getJSON( url, function(data) {
+        
+                        if ( isString( data ) ){
+                            
+                            console.log( 'version data :' + data );
+                            
+                            if (data.length > 12){
+                                data = data.substr(0,11)+'&hellip;';
+                                console.log( 'version data shorten to :' + data );
+                            }
+                            
+                            $('.version').text( data );
+                        }
+                    });
+}
+
+if (!isLocal()) init_common();
